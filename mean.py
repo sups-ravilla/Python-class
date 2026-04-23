535ab0696408e7a3e82e61fc8b714648fa3e681d
sups-ravilla <supratheekcravilla@gmail.com>
2026-04-23 18:32:50 +0100

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,15 @@
+mean1 = 38
+wrong = 36
+right= 56
+total = 40
+
+sum = mean1 * total
+print("The sum of the 40 numbers are: ",sum)
+
+
+num2 =sum-((wrong)-(right))
+print("sum-((wrong)-(right)): ",num2)
+
+
+mean2=num2/total
+print(mean2)
