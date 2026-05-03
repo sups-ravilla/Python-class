fed9d1fa976b4186c6ca8d7f9a29e2f6a12fee41
sups-ravilla <supratheekcravilla@gmail.com>
2026-05-03 11:29:23 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,39 @@
+print("Hello there.Please enter your name down below:")
+name = str(input("Name:"))
+print("Hello " + name + " please tell me your age to enroll in this course:")
+age = int(input("Age:"))
+if age <= 10:
+    print("Sorry " + name + " you are too young to enroll in this course.")
+else:
+    if age == 11:
+        print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+    else:
+        if age == 12:
+            print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+        else:
+            if age == 13:
+                print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+            else:
+                if age == 14:
+                    print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                else:
+                    if age == 15:
+                        print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                    else:
+                        if age == 16:
+                            print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                        else:
+                            if age == 17:
+                                print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                            else:
+                                if age == 18:
+                                    print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                                else:
+                                    if age == 19:
+                                        print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                                    else:
+                                        if age == 20:
+                                            print("Well done " + name + " you are old enough to enroll in this course.Please meet me in my office")
+                                        else:
+                                            if age >= 21:
+                                                print("I'm sorry " + name + " you are too old to enroll in this course.")    
