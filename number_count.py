1b838f1d4e2fbd438a9664a35b8cb2e51c60b502
sups-ravilla <supratheekcravilla@gmail.com>
2026-05-19 19:09:02 +0100

    Update number_count.py

--- number_count.py
+++ number_count.py
@@ -5,6 +5,5 @@ cleaned_num_str = ''.join(filter(str.isdigit, num_str))
 if not cleaned_num_str:
     print("No digits found in your input.")
 else:
-    # Calculate the number of digits
     num_digits = len(cleaned_num_str)
     print(f"The number {num_str} has {num_digits} digits.")
