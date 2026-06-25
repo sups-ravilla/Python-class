e684fccc1e804a280add4919719f2c8caa3a0f68
sups-ravilla <supratheekcravilla@gmail.com>
2026-06-25 18:22:24 +0100

    Add files via upload

---
+++ wellwishes.py
@@ -0,0 +1,8 @@
+def well_wishes():
+
+    print("Hello")
+    print("How are you?")
+    print("I hope you are enjoying your new secondary school!")
+well_wishes()
+print("Thank you for using this function.Have a nice day!")
+        
