9a1dcc9a39aaf26acb80e2932ffe57d8275c5ac3
sups-ravilla <supratheekcravilla@gmail.com>
2026-06-25 18:29:07 +0100

    Add files via upload

---
+++ weathercondition.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print("The weather is pleasant in:", spring)
+    print("The weather is the same in:", autumn)
+
+spring = "Autumn"
+autumn = spring    
+weather_condition()
+print("Thank you for using this program. Have a nice day!")
