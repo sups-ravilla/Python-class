ee26aa08d4c10b4527d6ed1e9ecad294b7202613
sups-ravilla <supratheekcravilla@gmail.com>
2026-07-16 18:20:08 +0100

    Add files via upload

---
+++ A.py
@@ -0,0 +1,12 @@
+
+a = input("Enter a word:")
+
+for i in a:
+    if (i == 'A'):
+
+     print("A is present in the word")
+     break
+    else:
+       print("A is not present in the word") 
+
+print("Thank you for using this program and have a nice day.")
