0e6d244b294f23eca8b55493606d1ed067c1c7b5
sups-ravilla <supratheekcravilla@gmail.com>
2026-08-13 18:27:14 +0100

    Add files via upload

---
+++ listfunctions.py
@@ -0,0 +1,12 @@
+empty_list = []
+print()
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers)
+
+triple = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]*3
+print(triple)
+
+cent = [100, 200, 300, 400, 500]
+cent = cent[::-1]
+print(cent,"\n")
